585052a1cdb3a3e4dd7ff8504ce377973f0ea243
John Montero <jmonteroc@gmail.com>
2011-11-09 14:26:31 -0500

    add subcommands

---
+++ dozo/commands/dozo-create.py
@@ -0,0 +1,48 @@
+from dozo.util      import get_commands
+from dozo.config    import get_config_value
+from dozo.commands  import BaseCommand, CommandError
+
+
+TEMPLATE_SUBCOMMAND = '''
+from dozo.config    import get_config_value
+from dozo.commands  import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    """
+    %s description.
+    """
+    
+    # command information
+    usage = '--%s'
+    summary = __doc__.strip().splitlines()[0].rstrip('.')
+     
+    def handle(self):
+        print self.usage
+'''
+
+class Command(BaseCommand):
+    """
+    Dozo create subcommand
+    """
+    
+    # command information
+    usage = '--dozo-create name_subcommand'
+    summary = __doc__.strip().splitlines()[0].rstrip('.')
+     
+    def handle(self):
+
+        value = self.args.get_value('--dozo-create')
+        if value is not None:
+            if value.endswith('.py'):
+                raise CommandError("\nNot include '.py'\n")
+
+            if value in get_commands() or value in get_commands(extend=True):
+                raise CommandError("\nSubcommand %s exist.\n" % value)
+
+            filename = '%s/commands/%s.py' % ( get_config_value('dozo-extend'),
+                                               value )
+            f = open(filename,'w+')    
+            f.write(TEMPLATE_SUBCOMMAND)
+            f.close()
+        else:
+            print "Please use options\n dozo %s\n" % self.usage
--- dozo/commands/dozo-extend.py
+++ dozo/commands/dozo-extend.py
@@ -4,24 +4,27 @@ from os import path
 
 class Command(BaseCommand):
     """
-    Dozo extension subcommand.
+    Path to extend subcommand.
     """
     
     # command information
-    usage = '-dozo-ext /path/to/subcommand'
+    usage = '--dozo-extend /path/to/extend/subcommand'
     summary = __doc__.strip().splitlines()[0].rstrip('.')
      
     def handle(self):
         
         conf = db.stored_config()        
-        value = self.args.get_value('--dozo-ext')
+        value = self.args.get_value('--dozo-extend')
         if value is None:
             try:
-                del conf['dozo-ext'] 
+                del conf['dozo-extend'] 
             except KeyError:
                 pass
         else:
+            if '-' in '/'.join(value.split('/')[-1:]):
+                raise CommandError("\nIs not character valid '-'.\n")
+
             if path.isdir(value):
-                conf['dozo-ext'] = value
+                conf['dozo-extend'] = value
             else:
-                raise CommandError("Is not directory %s" % value)
+                raise CommandError("\nIs not directory %s\n" % value)
